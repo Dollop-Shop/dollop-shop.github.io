94634fd42dccf4c5b06d001b12ceb7191a9cb4cf
Anson Kao <anson.kao@gmail.com>
2019-10-20 10:14:32 -0400

    Updates

---
+++ precache-manifest.d6d5a89e3c14866364cdacf692766832.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e914305742198cfd5a8346fe0f70cadf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894d4b72bf2b73f42839",
+    "url": "/static/css/main.c48c797d.chunk.css"
+  },
+  {
+    "revision": "b4d4aa8fbef4820e2390",
+    "url": "/static/js/2.47346638.chunk.js"
+  },
+  {
+    "revision": "894d4b72bf2b73f42839",
+    "url": "/static/js/main.46ee796a.chunk.js"
+  },
+  {
+    "revision": "92e4454e4fafc9359706",
+    "url": "/static/js/runtime-main.b41cc89f.js"
+  },
+  {
+    "revision": "27dc587a2c6f8c62b8993fb6de387663",
+    "url": "/static/media/dollop-bottle.27dc587a.png"
+  },
+  {
+    "revision": "96fdc43e1deb366f8bc17e4d38c24fb1",
+    "url": "/static/media/instagram.96fdc43e.svg"
+  },
+  {
+    "revision": "b3b64b1aec676ed812d8d06a0484c9fb",
+    "url": "/static/media/youtube.b3b64b1a.svg"
+  }
+]);
