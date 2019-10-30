7b85308ddb90f5d23f62a36feca4bc52f260e383
Anson Kao <anson.kao@gmail.com>
2019-10-30 11:23:18 -0400

    Zombie Hand image, Google Analytics, Finalized YouTube link

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
+import zombieHand from './zombie-hand-web.png';
 import dollopBottle from './dollop-bottle.png';
 import './LandingPage.less';
 import instagramImg from './instagram.svg';
 import youtubeImg from './youtube.svg';
 const INSTAGRAM = 'https://instagram.com/dollopskincare';
-const YOUTUBE = 'https://www.youtube.com/channel/UC63_4iT5ygk8JnyAyM8OoHw';
+const YOUTUBE = 'https://www.youtube.com/watch?v=Kz-joERX5AY';
 
 
 const MAILCHIMP_URL = "//store.us20.list-manage.com/subscribe/post?u=d2ccb41196286a704a6188eeb&amp;id=f7cde64b12";
@@ -14,12 +15,13 @@ const MAILCHIMP_URL = "//store.us20.list-manage.com/subscribe/post?u=d2ccb411962
 function LandingPage() {
   return (
     <div className="landing-page">
+      <img src={zombieHand} alt="Zombie Hand" className="zombie-hand" />
       <div className="landing-page-a">
         <img src={dollopBottle} alt="Dollop Lotion Bottle" />
       </div>
       <div className="landing-page-b">
-        <h1>Bring your skin</h1>
-        <h1>back to life</h1>
+        <h1>Revitalize&nbsp;</h1>
+        <h1>your skin</h1>
         <p>
           Dollop is a new skincare company launching soon.
           Be the first to know when our products become available.
